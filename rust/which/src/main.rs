3b0e36f98669761a08bc4c70d226b4aba082d4eb
Georgios Angelopoulos <YOUR_GITHUB_ID@users.noreply.github.com>
2025-07-09 23:28:29 +0200

    implement basic 'which' command functionality to locate executables in PATH

--- rust/which/src/main.rs
+++ rust/which/src/main.rs
@@ -1,3 +1,31 @@
+use std::{env, path::Path};
+
+fn get_seperator() -> char {
+    if cfg!(windows) { ';' } else { ':' }
+}
+
+fn get_path(program: &String) -> Option<String> {
+    if let Ok(path_variables) = env::var("PATH") {
+        for path in path_variables.split(get_seperator()) {
+            let executable_path = Path::new(path).join(program);
+            if executable_path.exists() {
+                return Some(executable_path.to_string_lossy().to_string());
+            }
+        }
+    };
+    None
+}
+
+const USAGE: &str = "Usage: which program ...";
 fn main() {
-    println!("Hello, world!");
+    let arguments: Vec<String> = env::args().collect();
+    if arguments.len() < 2 {
+        println!("{}", USAGE);
+        return;
+    }
+    let program = &arguments[1];
+    match get_path(program) {
+        Some(path) => println!("{}", path),
+        None => println!("{} not found in PATH", program),
+    }
 }
