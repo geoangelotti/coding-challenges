88241734ecfa18a6d5a5475f9887ef00cf467b4b
Georgios Angelopoulos <YOUR_GITHUB_ID@users.noreply.github.com>
2025-07-27 00:40:23 +0200

    handle character solution

---
+++ js/wc/index.ts
@@ -0,0 +1,60 @@
+import { argv } from "node:process";
+import { readFile, access } from "fs/promises";
+import { constants } from "fs";
+import { Result, ok, err } from "neverthrow";
+
+type CliFlag = { type: "Chars"; count: (b: Buffer) => number };
+
+const CliFlag = {
+  Chars: (): CliFlag => ({
+    type: "Chars",
+    count: (b: Buffer) => b.length,
+  }),
+};
+
+const USAGE = "USAGE: wc -[lmwc] file";
+
+function parseArguments(args: string[]): Result<[string, CliFlag], string> {
+  if (args.length < 4) {
+    return err(USAGE);
+  }
+  let cliFlag: CliFlag;
+  switch (args[2]) {
+    case "-c":
+      cliFlag = CliFlag.Chars();
+      break;
+
+    default:
+      return err(USAGE);
+  }
+  return ok([args[3], cliFlag]);
+}
+
+async function read(path: string): Promise<Result<Buffer, string>> {
+  try {
+    await access(path, constants.F_OK | constants.R_OK);
+    const data = await readFile(path);
+    return ok(data);
+  } catch (error: any) {
+    if (error.code === "ENOENT") {
+      return err(`${path} does not exist`);
+    }
+    if (error.code === "EACCES") {
+      return err(`Permission denied: ${path}`);
+    }
+    return err(error.message || String(error));
+  }
+}
+
+async function main() {
+  const argsResult = parseArguments(argv);
+  if (argsResult.isErr()) {
+    console.log(argsResult.mapErr((err) => err));
+    return;
+  }
+  const [path, cliFlag] = argsResult._unsafeUnwrap();
+  const buffer = (await read(path))._unsafeUnwrap();
+  console.log(`${CliFlag.Chars().count(buffer)} ${path}`);
+}
+
+await main();
